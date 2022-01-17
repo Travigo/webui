8c66b926d37eabac0e7e3e278b91277dd424f7bc
Aaron Claydon <claydon.aaron@gmail.com>
2022-01-17 14:38:39 +0000

    Change public URL

--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
 export default {
-  URL: (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://api.britbus.claydonlee.com')
+  URL: (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://api.britbus.devspace.app')
 }
