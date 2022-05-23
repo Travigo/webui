1e895accfb8301c9667e5618e2f0cef39d2dca60
Aaron Claydon <claydon.aaron@gmail.com>
2022-05-23 18:10:57 +0100

    Service icon as own component

---
+++ src/components/ServiceIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="text-base text-center font-semibold inline-block py-1 px-2 uppercase rounded text-white bg-blue-200 h-8 min-w-[1rem]"
+    :style="[service.BrandColour ? {'background': service.BrandColour} : {}]"
+  >
+    {{ service.ServiceName }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    service: {},
+  }
+}
+</script>
