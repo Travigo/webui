e49f06ce737c33781576a617ee2b212be339fcd7
Aaron Claydon <claydon.aaron@gmail.com>
2022-08-22 15:41:18 +0100

    Update pretty.js

--- src/pretty.js
+++ src/pretty.js
@@ -1,12 +1,6 @@
 export default {
-  parseIncorrectDateTime(datetimeString) {
-    let datetime = new Date(datetimeString)
-    let userTimezoneOffset = datetime.getTimezoneOffset() * 60000
-    
-    return new Date(datetime.getTime() + userTimezoneOffset)
-  },
   time(datetimeString) {
-    let datetime = this.parseIncorrectDateTime(datetimeString)
+    let datetime = new Date(datetimeString)
 
     let hours = this.padToTwo(datetime.getHours())
     let minutes = this.padToTwo(datetime.getMinutes())
@@ -14,7 +8,7 @@ export default {
     return `${hours}:${minutes}`
   },
   day(datetimeString) {
-    let datetime = this.parseIncorrectDateTime(datetimeString)
+    let datetime = new Date(datetimeString)
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     return days[datetime.getDay()]
