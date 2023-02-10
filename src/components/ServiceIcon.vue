437ae73105d629ca6d437d5684db214d09bba2a6
Aaron Claydon <claydon.aaron@gmail.com>
2023-02-10 15:27:06 +0000

    Fix service icon

--- src/components/ServiceIcon.vue
+++ src/components/ServiceIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-base text-center font-semibold inline-block py-1 px-2 uppercase rounded text-white bg-blue-200 h-8 min-w-[1rem]"
+    class="text-base text-center font-semibold inline-block py-0 px-2 uppercase rounded text-white bg-blue-200 min-h-8 min-w-[1rem]"
     :style="[service.BrandColour ? {'background': service.BrandColour} : {}]"
   >
     {{ service.ServiceName }}
