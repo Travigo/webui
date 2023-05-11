2823d263078654d3ce2670686db890e4bd8460ff
Aaron Claydon <claydon.aaron@gmail.com>
2023-05-11 21:55:05 +0100

    Optional link to journey view

--- src/views/Stops/View.vue
+++ src/views/Stops/View.vue
@@ -72,27 +72,13 @@
                 </div>
               </div>
               <div class="my-auto text-right">
-                <router-link :to="{'name': 'journeys/view', params: {'id': departure.Journey.PrimaryIdentifier}}">
-                  {{ this.pretty.time(departure.Time) }}
-                  <div 
-                    class="text-xs text-green-700" 
-                    v-if="departure.Type == 'RealtimeTracked'"
-                    :class="{ 'text-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
-                  >
-                    <div 
-                      class="inline-flex items-center justify-center w-1 h-1 text-xs bg-green-700 rounded-full"
-                      :class="{ 'bg-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
-                    >&nbsp;</div>
-                    Realtime 
-                  </div>
-                  <div class="text-xs text-gray-600 dark:text-gray-400" v-else-if="departure.Type == 'Scheduled'">
-                    Scheduled
-                  </div>
-                   <div class="text-xs text-orange-500" v-else-if="departure.Type == 'Estimated'">
-                    <div class="inline-flex items-center justify-center w-1 h-1 text-xs bg-orange-500 rounded-full">&nbsp;</div>
-                    Estimated
-                  </div>
+                <router-link 
+                  :to="{'name': 'journeys/view', params: {'id': departure.Journey.PrimaryIdentifier}}" 
+                  v-if="departure.Journey.PrimaryIdentifier !=''"
+                >
+                  <DepartureTimeView :departure="departure" />
                 </router-link>
+                <DepartureTimeView :departure="departure" v-else />
               </div>
             </div>
           </div>
@@ -127,6 +113,7 @@ import PageTitle from '@/components/PageTitle.vue'
 import Card from '@/components/Card.vue'
 import ServiceIcon from '@/components/ServiceIcon.vue'
 import Alert from '@/components/Alert.vue'
+import DepartureTimeView from '@/components/Stops/DepartureTimeView.vue'
 import axios from 'axios'
 import API from '@/API'
 import Pretty from '@/pretty'
@@ -158,6 +145,7 @@ export default {
     Card,
     ServiceIcon,
     Alert,
+    DepartureTimeView,
   },
   methods: {
     getStop() {
