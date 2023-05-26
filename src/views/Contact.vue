04a5b8dc962a681993d5dd68a9ba5acc43668931
Aaron Claydon <claydon.aaron@gmail.com>
2023-05-26 20:05:43 +0100

    Update britbus references to travigo

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,8 +9,8 @@ import Card from '@/components/Card.vue'
       Contact Us
     </PageTitle>
     <Card>
-      You can contact me by email at <a href="mailto:aaron@britbus.app" class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
-        aaron@britbus.app
+      You can contact me by email at <a href="mailto:aaron@travigo.app" class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
+        aaron@travigo.app
       </a>
     </Card>
   </div>
