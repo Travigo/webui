d48bf74dddda137bd569da925c856d6b64f78d47
Aaron Claydon <claydon.aaron@gmail.com>
2023-06-14 22:41:48 +0100

    Slightly improved journey planner UI

--- src/views/Planner/Search.vue
+++ src/views/Planner/Search.vue
@@ -4,41 +4,54 @@
       Search Results
     </PageTitle>
     <Card>
+      <span 
+        v-if="this.loadingResults"
+        class="text-xs font-semibold inline-block py-1 px-2 rounded text-amber-600 bg-amber-200 mr-1"
+      >
+        Loading Results
+      </span>
       <div class="mb-4 last:mb-0 " v-for="(journeyPlan, index) in this.journeyPlans" v-bind:key="index">
+        <div class="block text-center text-xs mb-4 text-gray-400" v-if="this.departureDayChange(index)">
+          {{ this.pretty.day(journeyPlan.StartTime) }}
+        </div>
         <div class="flex">
           <div class="flex-auto my-auto">
-            <div class="flex w-[30rem]">
-              <div class="flex-auto my-auto">
-                <div class="text-lg">{{ this.pretty.time(journeyPlan.StartTime) }}</div>
-                <div class="text-sm">Scheduled</div>
-              </div>
+            <div class="flex">
+              <div>
+                <div class="inline-block w-[5rem] text-center">
+                  <div class="text-xl font-black">
+                    {{ this.pretty.time(journeyPlan.StartTime) }}
+                  </div>
+                  <div class="text-xs font-light">Scheduled</div>
+                </div>
 
-              <div class="flex-auto my-auto">
-               ->  
-              </div>
+                <div class="inline-block">
+                ->  
+                </div>
 
-              <div class="flex-auto my-auto">
-                <div class="text-lg">{{ this.pretty.time(journeyPlan.ArrivalTime) }}</div>
-                <div class="text-sm">Scheduled</div>
-              </div>
-              
-              <div class="flex-auto my-auto">
-                ({{ this.pretty.duration(journeyPlan.Duration) }})
-                <span 
-                  v-if="index==earliestArrivalJourneyID"
-                  class="text-xs font-semibold inline-block py-1 px-2 rounded text-amber-600 bg-amber-200 mr-1"
-                >
-                  Earliest
-                </span>
+                <div class="inline-block w-[5rem] text-center">
+                  <div class="text-xl font-black">
+                    {{ this.pretty.time(journeyPlan.ArrivalTime) }}
+                  </div>
+                  <div class="text-xs font-ligh">Scheduled</div>
+                </div>
+                
+                <div class="inline-block">
+                  <span 
+                    v-if="index==earliestArrivalJourneyID"
+                    class="text-xs font-semibold inline-block py-1 px-2 rounded text-blue-600 bg-blue-200 mr-1"
+                  >
+                    Earliest
+                  </span>
+                </div>
               </div>
             </div>
             <div class="text-xs">
-              <span v-if="journeyPlan.RouteItems.length==1">
-                Direct
-              </span>
-              <span v-else>
-                {{ journeyPlan.RouteItems.length-1 }} changes
-              </span>
+               to {{ journeyPlan.RouteItems[0].Journey.DestinationDisplay }}
+            </div>
+            <div class="text-xs">
+              <span v-if="journeyPlan.RouteItems.length==1">Direct</span>
+              <span v-else>{{ journeyPlan.RouteItems.length-1 }} changes</span>, {{ this.pretty.duration(journeyPlan.Duration) }}
             </div>
           </div>
           <div class="my-auto text-right">
@@ -67,7 +80,7 @@ export default {
     return {
       pretty: Pretty,
       journeyPlans: [],
-      loadingResult: true,
+      loadingResults: true,
       earliestArrivalJourneyID: 0
     }
   },
@@ -96,6 +109,20 @@ export default {
         })
         .finally(() => this.loadingResults = false)
     },
+    departureDayChange(index) {
+      let comparisonDateTime;
+      // If we're at the start then comparison datetime is current date else its the last items
+      // TODO when able to look in future handle that
+      if (index == 0) {
+        comparisonDateTime = new Date(Date.now())
+      } else {
+        comparisonDateTime = new Date(Date.parse(this.journeyPlans[index-1].StartTime))
+      }
+
+      let currentDateTime = new Date(Date.parse(this.journeyPlans[index].StartTime))
+
+      return comparisonDateTime.getDate() != currentDateTime.getDate()
+    }
   },
   mounted () {
     console.log(this.$route.query)
