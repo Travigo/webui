484752e81d223ffa180ff6a59fa4016bc06067dc
Aaron Claydon <claydon.aaron@gmail.com>
2023-08-02 22:21:04 +0100

    National Rail icon & contrast text for service icon

--- src/components/ServiceIcon.vue
+++ src/components/ServiceIcon.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="text-sm text-center inline-block py-0 px-2 rounded text-white bg-blue-200 min-h-8 min-w-[1rem]"
+    class="text-sm text-center inline-block py-0 px-2 rounded bg-blue-200 min-h-8 min-w-[1rem]"
+    :class="{'text-gray-800': darkText, 'text-white': !darkText}"
     :style="[service.BrandColour ? {'background': service.BrandColour} : {}]"
     v-if="service.ServiceName != '' || service.BrandIcon != ''"
   >
@@ -26,6 +27,19 @@ export default {
     service: {},
     short: {
       default: false
+    }
+  },
+  computed: {
+    darkText() {
+      // var color = Math.round(((parseInt(rgbValue[0]) * 299) +
+      //       (parseInt(rgbValue[1]) * 587) +
+      //       (parseInt(rgbValue[2]) * 114)) / 1000);
+
+      var color = (this.service.BrandColour.charAt(0) === '#') ? this.service.BrandColour.substring(1, 7) : this.service.BrandColour
+      var r = parseInt(color.substring(0, 2), 16)
+      var g = parseInt(color.substring(2, 4), 16)
+      var b = parseInt(color.substring(4, 6), 16)
+      return ((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186
     }
   }
 }
