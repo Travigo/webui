1b09fc76c957966b29f76774fbf9b033767e63cb
Aaron Claydon <claydon.aaron@gmail.com>
2023-08-25 23:28:18 +0100

    Cancelled text

--- src/components/Stops/DepartureTimeView.vue
+++ src/components/Stops/DepartureTimeView.vue
@@ -1,27 +1,34 @@
 <template>
-  {{ this.pretty.time(departure.Time) }}
-  <div 
-    class="text-xs text-green-700" 
-    v-if="departure.Type == 'RealtimeTracked'"
-    :class="{ 'text-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
-  >
+  <span v-if="departure.Type == 'Cancelled'">
+    <div class="text-xs text-red-600 dark:text-red-400">
+      Cancelled
+    </div>
+  </span>
+  <span v-else>
+    {{ this.pretty.time(departure.Time) }}
     <div 
-      class="inline-flex items-center justify-center w-1 h-1 text-xs bg-green-700 rounded-full"
-      :class="{ 'bg-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
-    >&nbsp;</div>
-    Realtime 
-  </div>
-  <div class="text-xs text-gray-600 dark:text-gray-400" v-else-if="departure.Type == 'Scheduled'">
-    Scheduled
-  </div>
-    <div class="text-xs text-orange-500" v-else-if="departure.Type == 'Estimated'">
-    <div class="inline-flex items-center justify-center w-1 h-1 text-xs bg-orange-500 rounded-full">&nbsp;</div>
-    Estimated
-  </div>
+      class="text-xs text-green-700" 
+      v-if="departure.Type == 'RealtimeTracked'"
+      :class="{ 'text-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
+    >
+      <div 
+        class="inline-flex items-center justify-center w-1 h-1 text-xs bg-green-700 rounded-full"
+        :class="{ 'bg-orange-400': (departure.Journey.RealtimeJourney?.Reliability == 'LocationWithoutTrack') }"
+      >&nbsp;</div>
+      Realtime 
+    </div>
+    <div class="text-xs text-gray-600 dark:text-gray-400" v-else-if="departure.Type == 'Scheduled'">
+      Scheduled
+    </div>
+      <div class="text-xs text-orange-500" v-else-if="departure.Type == 'Estimated'">
+      <div class="inline-flex items-center justify-center w-1 h-1 text-xs bg-orange-500 rounded-full">&nbsp;</div>
+      Estimated
+    </div>
 
-  <div class="text-xs text-gray-800" v-if="departure.Platform">
-    Platform {{ departure.Platform }} <span v-if="departure.PlatformType != 'ACTUAL'">(Est.)</span>
-  </div>
+    <div class="text-xs text-gray-800" v-if="departure.Platform">
+      Platform {{ departure.Platform }} <span v-if="departure.PlatformType != 'ACTUAL'">(Est.)</span>
+    </div>
+  </span>
 </template>
 
 <script>
