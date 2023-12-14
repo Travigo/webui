26bd34eebc0eb1a34f4baf2d43620228811f9d12
Aaron Claydon <claydon.aaron@gmail.com>
2023-12-14 21:24:45 +0000

    Fix scrolling broken

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -96,7 +96,8 @@
       <PageTitle>Departures</PageTitle>
       <i>Click view more for departures</i>
       <p class="mt-2">
-        <router-link 
+        <router-link
+          @click="this.$refs.stopInfoSheet.close()"
           :to="{'name': 'stops/view', params: {'id': currentViewedStop.PrimaryIdentifier}}"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
